ebab795ffa8dcd77b182d74c4b89b4ccac02e1e2
Anatom <49984698+anatom3000@users.noreply.github.com>
2022-08-15 18:08:01 +0200

    fixed intents once again

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 class TuringBot(commands.Bot):
     __version__ = '1.0'
